79d415bb774bcdb7aebb69bc6a35758e60df7df2
azharrf <azhar.rizki.0208@gmail.com>
2021-11-03 00:15:29 +0700

    update function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+let url = ['https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/posts']
 const mainArticle = document.querySelector('.main-article');
 const titleArticle = document.getElementById('title-article');
 const bodyArticle = document.getElementById('body-article');
+const username = document.getElementById('username');
+const website = document.getElementById('website');
 const sbmtButton = document.querySelector('.sbmt-button');
-let titleContent = '';
-let bodyContent = '';
-let idPost = '';
-let idUser = '';
+const dltButton = document.querySelector('.btn-delete');
+const mdlPost = document.querySelector('.modal-foot-post');
+let user = url[0];
+let post = url[1];
+let id = '';
 
 // Menu Toggle
 document.addEventListener('click', function(e) {
@@ -14,42 +18,50 @@ document.addEventListener('click', function(e) {
     }
 });
 
-let url = ['https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/posts']
-
 Promise.all(url.map(u => fetch(u)))
         .then(response => Promise.all(response.map(res => res.json())))
         .then(data => showPost(data))
 
 function showPost(data){
-    console.log("data", data)
+    // console.log("data", data)
     const mainArticle = document.querySelector('.main-article');
+    let users = data[0];
+    let posts = data[1];
     let article = '';
 
-    for(i = 0; i < 10; i++){
-        for(x = 0; x < 10; x++){
-            if(i >= 1){
-                let result = x + (i * 10)
-                // article += result;
-                article += card(data[0][i], data[1][result])
-            } else {
-                // article += x
-                article += card(data[0][i], data[1][x])
+    posts.map(post => {
+        users.map(user => {
+            if(post.userId == user.id){
+                article += card(user, post)
             }
-            // console.log(data[0][i], data[1][x])
-        }
-    }
-    // console.log(article)
+        });
+    });
+
+    // for(i = 0; i < 10; i++){
+    //     for(x = 0; x < 10; x++){
+    //         if(i >= 1){
+    //             let result = x + (i * 10)
+    //             // article += result;
+    //             article += card(data[0][i], data[1][result])
+    //         } else {
+    //             // article += x
+    //             article += card(data[0][i], data[1][x])
+    //         }
+    //         // console.log(data[0][i], data[1][x])
+    //     }
+    // }
+    // // console.log(article)
     mainArticle.innerHTML = article;
 }
 
 function card(user, post){
-    // console.log('card', user)
+    // console.log('card', user, post)
     // Limit Word
     let titleText = post.title.slice(0, 70) + (post.title.length > 70 ? "..." : " ")
     let bodyText = post.body.slice(0, 180) + (post.body.length > 180 ? "..." : " ")
 
     return `<div class="col card-article">
-                <div class="card"  data-user="${user.id}" data-post="${post.id}">
+                <div class="card" data-post="${post.id}">
                     <div class="settings">
                         <i class='bx bx-dots-vertical-rounded menu-toggle'></i>
                         <div class="set">
@@ -70,7 +82,7 @@ function card(user, post){
                             </div>
                             <div class="name">
                                 <h5 class="username">${user.name}</h5>
-                                <span class="email">${user.website}</span>
+                                <span class="website">${user.website}</span>
                             </div>
                         </div>
                     </div>
@@ -81,34 +93,79 @@ function card(user, post){
 mainArticle.addEventListener('click', (e) => {
     // console.log(e.target.classList)
     var parent = e.target.parentElement.parentElement.parentElement
-    let editButton = (e.target.id == "edit-post" || e.target.classList.contains('bxs-edit'));
-    let deleteButton = (e.target.id == "delete-post" || e.target.classList.contains('bxs-trash'));
-    idUser = parent.dataset.user;
-    idPost = parent.dataset.post;
+    let editButton = (e.target.id == "edit-post");
+    id = parent.dataset.post;
 
-    // // Edit
+    // Edit
     if(editButton){
-        titleContent = parent.querySelector('.title-post').textContent;
-        bodyContent = parent.querySelector('.body-post').textContent;
+        let titleContent = parent.querySelector('.title-post').textContent;
+        let bodyContent = parent.querySelector('.body-post').textContent;
+        let userName = parent.querySelector('.username').textContent;
+        let webSite = parent.querySelector('.website').textContent;
 
         titleArticle.value = titleContent;
         bodyArticle.value = bodyContent;
+        username.value = userName;
+        website.value = webSite;
     }
+
+    // Update
+    sbmtButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        // console.log(`${id}`)
+        fetch(`${post}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: titleArticle.value,
+                body: bodyArticle.value,
+            })
+
+        })
+            .then(res => res.json())
+            .then(sbmtButton.setAttribute('data-bs-dismiss', 'modal'))
+        titleArticle.value = '';
+        bodyArticle.value = '';
+        username.value = '';
+        website.value = '';
+    });
 });
 
-// Update
-sbmtButton.addEventListener('click', () => {
-    // console.log(`${url[1]}`)
-    fetch(`${url[1]}/${idPost}`, {
-        method: 'PUT',
+// Add Post
+mdlPost.addEventListener('click', (e) => {
+    // console.log('halo')
+    fetch(url, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             title: titleArticle.value,
-            body: bodyArticle.value,
+            body: bodyArticle.value
+            // name: username.value,
+            // website: website.value,
         })
     })
         .then(res => res.json())
-        .then(json => console.log(json))
-})
+        .then(data => {
+            console.log(data)
+            // const dataArr = [];
+            // dataArr.push(data);
+            // renderPosts(dataArr);
+        })
+});
+
+// Delete
+dltButton.addEventListener('click', () => {
+    fetch(`${post}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(document.addEventListener('click', (e) => {
+            if(e.target.classList.contains('btn-delete')){
+                dltButton.setAttribute('data-bs-dismiss', 'modal')
+            }
+        }))
+});
